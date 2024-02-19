fee3e46e28b103d59d68018fd82cc6ac3916a9e0
Bestian Tang 大巴 <bestian@gmail.com>
2024-02-19 08:07:00 +0800

    init

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,16 @@
 module Lib
-    ( someFunc
+    ( Quiz rollDice, rollDice2
     ) where
+
+import System.Random 
+
+rollDice :: IO Quiz
+rollDice = do x <- getStdRandom (randomR (10,20))
+              return $ Lit x
+
+rollDice2 :: IO Quiz
+rollDice2 = do x <- getStdRandom (randomR (1,10))
+               return $ Lit x
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
