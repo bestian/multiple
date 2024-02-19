f99d6417ab51d371413c009ac30bd9ead138567b
Bestian Tang 大巴 <bestian@gmail.com>
2024-02-19 10:55:21 +0800

    s

--- app/Main.hs
+++ app/Main.hs
@@ -2,17 +2,13 @@ module Main (main) where
 
 import Lib
 
-makeTxt a 0 t = do return t
-makeTxt a n t = do t <- makeTxt a (n-1) t
-	return (show q ++ " = ?\n\n" ++ t)
-
 
 loop l s e = do x <- rollDice
-				y <- rollDice2
-	      		putStrLn "\n請問" ++ x "是" y 的倍數嗎 ++"? y]是 n]不是\n"
+                y <- rollDice2
+                putStrLn "\n請問" ++ x "是" y 的倍數嗎 ++"? y]是 n]不是\n"
                 myA <- getLine
-                let a = (read myA) :: String
-                if (ans == "y" && y `mod` x == 0)
+                let answer = (read myA) :: String
+                if (answer == "y" && y `mod` x == 0)
                    then do putStrLn $ "答對了！Geart!"
                            putStrLn $ "目前分數：" ++ show (s+1)
                            if s + 1 >= 10
@@ -28,4 +24,4 @@ loop l s e = do x <- rollDice
 main :: IO ()
 main = do putStrLn "\n歡迎來到倍數小測驗！\n"
           loop a 0 0
-	        
+            
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,13 +1,13 @@
 module Lib
-    ( Quiz rollDice, rollDice2
+    ( rollDice, rollDice2
     ) where
 
 import System.Random 
 
-rollDice :: IO Quiz
+rollDice :: IO Int
 rollDice = do x <- getStdRandom (randomR (10,20))
               return $ x
 
-rollDice2 :: IO Quiz
+rollDice2 :: IO Int
 rollDice2 = do x <- getStdRandom (randomR (1,10))
                return $ x
