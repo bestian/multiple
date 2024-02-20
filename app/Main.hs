0cedc68ce742573a2f8dafb0c7e8afb8a9f50646
Bestian Tang 大巴 <bestian@gmail.com>
2024-02-20 11:21:41 +0800

    Update Main.hs
    v1.0

--- app/Main.hs
+++ app/Main.hs
@@ -3,25 +3,25 @@ module Main (main) where
 import Lib
 
 
-loop l s e = do x <- rollDice
-                y <- rollDice2
-                putStrLn "\n請問" ++ x "是" y 的倍數嗎 ++"? y]是 n]不是\n"
-                myA <- getLine
-                let answer = (read myA) :: String
-                if (answer == "y" && y `mod` x == 0)
-                   then do putStrLn $ "答對了！Geart!"
-                           putStrLn $ "目前分數：" ++ show (s+1)
-                           if s + 1 >= 10
-                             then do putStrLn $ "你勝利了！\n\n"
-                                     main
-                             else loop l (s + 1) e
-                else
-                   do putStrLn $ "答錯了，沒關係，再接再勵。答案是" ++ show ans
-                      if e + 1 >= 3
-                        then main
-                      else loop l s (e + 1)
+loop :: Int -> Int -> IO ()
+loop s e = do x <- rollDice
+              y <- rollDice2
+              putStrLn $ "\n請問" ++ show x ++ "是" ++ show y ++ "的倍數嗎? y)是 n)不是\n"
+              myA <- getLine
+              if (myA == "y" && x `mod` y == 0) || (myA == "n" && x `mod` y /= 0)
+                 then do putStrLn "答對了！Great!"
+                         let newScore = s + 1
+                         putStrLn $ "目前分數：" ++ show newScore
+                         if newScore >= 10
+                           then putStrLn "你勝利了！\n\n" >> main
+                           else loop newScore e
+                 else do putStrLn "答錯了，沒關係，再接再勵。"
+                         let newErrors = e + 1
+                         if newErrors >= 3
+                           then putStrLn "遊戲結束，再來一次吧！\n\n" >> main
+                           else loop s newErrors
 
 main :: IO ()
 main = do putStrLn "\n歡迎來到倍數小測驗！\n"
-          loop a 0 0
+          loop 0 0
             
