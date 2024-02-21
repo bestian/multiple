b45b8a353d147c8505bb63fdb127fd28493cda3f
Bestian Tang <bestian@gmail.com>
2024-02-21 14:47:34 +0800

    Update Lib.hs

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,9 +5,9 @@ module Lib
 import System.Random 
 
 rollDice :: IO Int
-rollDice = do x <- getStdRandom (randomR (10,20))
+rollDice = do x <- getStdRandom (randomR (10,40))
               return $ x
 
 rollDice2 :: IO Int
-rollDice2 = do x <- getStdRandom (randomR (1,10))
+rollDice2 = do x <- getStdRandom (randomR (1,5))
                return $ x
